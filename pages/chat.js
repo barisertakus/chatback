3887353bd37045582166dedbf4234de6b5e5c9b1
Barış Ertakuş <bariserta@gmail.com>
2022-04-12 20:50:16 +0300

    Update chat page with grids

--- pages/chat.js
+++ pages/chat.js
@@ -3,13 +3,20 @@ import Navbar from "../components/Navbar";
 import styled from "styled-components";
 import Sidebar from "../components/Sidebar";
 import Messages from "../components/Messages";
+import { Grid } from "@mui/material";
 
 function Chat() {
   return (
     <Container>
       <Content>
-        <Sidebar />
-        <Messages />
+        <Grid container spacing={2}>
+          <Grid item xs={12} xl={3} lg={4} md={5} >
+            <Sidebar />
+          </Grid>
+          <Grid item xs={12} xl={9} lg={8} md={7}>
+            <Messages />
+          </Grid>
+        </Grid>
       </Content>
     </Container>
   );
@@ -19,10 +26,9 @@ export default Chat;
 
 const Container = styled.div`
   background-color: #eff6fc;
+  height: 100%;
 `;
 
 const Content = styled.div`
-  display: flex;
-  flex-wrap: wrap;
   padding: 20px;
 `;
