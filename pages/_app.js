3e2bbd8f73dd8e274e396c7ee40262589c3b960f
Barış Ertakuş <bariserta@gmail.com>
2022-04-14 04:00:03 +0300

    Create appContext for socket.io states
    React context has been created to keep all created states for socket.io.

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,45 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/lib/integration/react";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import store from "../app/store";
+import { AppContext, socket } from "../context/appContext";
+import { useState } from "react";
 
 const persistedStore = persistStore(store);
 
 function MyApp({ Component, pageProps }) {
+  const [rooms, setRooms] = useState([]);
+  const [currentRoom, setCurrentRoom] = useState({});
+  const [members, setMembers] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [privateMemberMessage, setPrivateMemberMessage] = useState({});
+  const [newMessages, setNewMessages] = useState({});
+
+  const providerValue = {
+    socket,
+    rooms,
+    setRooms,
+    messages,
+    setMessages,
+    newMessages,
+    setNewMessages,
+    currentRoom,
+    setCurrentRoom,
+    members,
+    setMembers,
+    privateMemberMessage,
+    setPrivateMemberMessage,
+  };
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistedStore}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <AppContext.Provider value={providerValue}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </AppContext.Provider>
       </PersistGate>
     </Provider>
   );
