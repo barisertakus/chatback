00f23ac4793eebb857ea15edd9bb1ffc6333d617
Barış Ertakuş <bariserta@gmail.com>
2022-04-18 16:36:42 +0300

    Add loading & redirect to chat

--- pages/chat.js
+++ pages/chat.js
@@ -1,7 +1,9 @@
 import { Grid } from "@mui/material";
-import React, { useContext, useEffect } from "react";
+import Router from "next/router";
+import React, { useContext, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import Loading from "../components/Loading";
 import Messages from "../components/Messages";
 import Sidebar from "../components/Sidebar";
 import { AppContext } from "../context/appContext";
@@ -9,26 +11,43 @@ import { URL } from "../environment";
 import { selectUser } from "../features/userSlice";
 
 function Chat({ rooms }) {
-  const { setRooms } = useContext(AppContext);
-
+  const { setRooms, loading } = useContext(AppContext);
+  const [checkUser, setCheckUser] = useState(false);
   const user = useSelector(selectUser);
 
   useEffect(() => {
     setRooms(rooms);
   }, [rooms, setRooms]);
 
+  useEffect(() => {
+    if (!user?._id) {
+      Router.push("/login").then(()=>{
+        setCheckUser(true);
+      });
+    } else {
+      setCheckUser(true);
+    }
+
+  }, []);
+
   return (
     <Container>
-      <Content>
-        <Grid container spacing={2}>
-          <Grid item xs={12} xl={3} lg={4} md={5}>
-            <Sidebar />
+      {!checkUser || loading ? (
+        <LoadingWrapper>
+          <Loading />
+        </LoadingWrapper>
+      ) : (
+        <Content>
+          <Grid container spacing={2}>
+            <Grid item xs={12} xl={3} lg={4} md={5}>
+              <Sidebar />
+            </Grid>
+            <Grid item xs={12} xl={9} lg={8} md={7}>
+              <Messages />
+            </Grid>
           </Grid>
-          <Grid item xs={12} xl={9} lg={8} md={7}>
-            <Messages />
-          </Grid>
-        </Grid>
-      </Content>
+        </Content>
+      )}
     </Container>
   );
 }
@@ -47,6 +66,12 @@ const Content = styled.div`
   @media (max-width: 407px) {
     padding: 7px;
   }
+`;
+
+const LoadingWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const getStaticProps = async () => {
